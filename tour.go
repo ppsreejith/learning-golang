5fe9fcedcc589ce7510aeafa405a5e1ba0fc5625
Sreejith Pp <mailz4sreejith@gmail.com>
2018-09-10 20:46:05 +0530

    Completed tour till chap 2

--- tour.go
+++ tour.go
@@ -1,7 +1,16 @@
+// gotour
+// http://127.0.0.1:3999/basics/7
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println(swap("hello", "world"))
 }
