9d99915163d95d1feaad611fa579142849814124
Sreejith Pp <mailz4sreejith@gmail.com>
2018-09-13 09:29:05 +0530

    Finished part 2 control flow statements in golang

--- tour.go
+++ tour.go
@@ -1,17 +1,16 @@
+//gotour
+//http://127.0.0.1:3999/moretypes/1
+
 package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	switch t := time.Now(); {
-	case t.Hour() < 12:
-		fmt.Println("Good morning!")
-	case t.Hour() < 17:
-		fmt.Println("Good afternoon!")
-	default:
-		fmt.Println("Good evening!")
+	defer fmt.Println("Counting")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
 	}
+	fmt.Println("done")
 }
