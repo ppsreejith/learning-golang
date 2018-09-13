aba3349e576a8cacd7857d75548b3851ca9e165f
Sreejith Pp <mailz4sreejith@gmail.com>
2018-09-13 09:13:44 +0530

    Completed half of go tour control flow

--- tour.go
+++ tour.go
@@ -1,22 +1,17 @@
-// gotour
-// http://127.0.0.1:3999/basics/7
-
 package main
 
 import (
 	"fmt"
+	"math"
 )
 
+func Sqrt(x float64) (root float64) {
+	for root = 1; math.Abs(root*root-x) > 0.001; {
+		root -= (root*root - x) / (2 * root)
+	}
+	return
+}
+
 func main() {
-	var (
-		ToBe   bool   = false
-		maxInt uint64 = 1<<64 - 1
-	)
-	var (
-		i int     = 42
-		f float64 = float64(i)
-		u uint    = uint(i)
-	)
-	const World = "Bhoomi"
-	fmt.Println(ToBe, maxInt, i, f, u, World)
+	fmt.Println(Sqrt(2))
 }
