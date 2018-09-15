5cdf68f9d22b195cfe3409a63c99fa77257ccdcd
Sreejith Pp <mailz4sreejith@gmail.com>
2018-09-16 00:46:55 +0530

    Finished golang slices and arrays

--- tour.go
+++ tour.go
@@ -1,21 +1,11 @@
 //gotour
-//http://127.0.0.1:3999/moretypes/1
+//http://127.0.0.1:3999/moretypes/13
 
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	i, j := 42, 2701
-
-	p := &i
-	fmt.Println(*p)
-	*p = 21
-	fmt.Println(i)
-
-	p = &j
-	*p = *p / 37
-	fmt.Println(j)
+	var s []int
+	fmt.Println(s, len(s), cap(s), s == nil)
 }
